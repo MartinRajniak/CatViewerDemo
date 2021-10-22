5f7224d30a2749f9f1efdaaa3d27507ee2301922
Martin Rajniak <rajniak.m@gmail.com>
2021-10-22 11:11:53 +0200

    Switch Android project to Jetpack Compose

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -3,11 +3,23 @@ plugins {
     kotlin("android")
 }
 
+val composeVersion by extra ("1.0.4")
+
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    // Compose
+    implementation("androidx.activity:activity-compose:1.3.1")
+
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.material:material-icons-core:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+
+    // UI Tests
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 }
 
 android {
@@ -18,6 +30,20 @@ android {
         targetSdkVersion(31)
         versionCode = 1
         versionName = "1.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
     }
     buildTypes {
         getByName("release") {
--- androidApp/src/main/java/eu/rajniak/cat/android/MainActivity.kt
+++ androidApp/src/main/java/eu/rajniak/cat/android/MainActivity.kt
@@ -1,20 +1,45 @@
 package eu.rajniak.cat.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import eu.rajniak.cat.Greeting
-import android.widget.TextView
+import eu.rajniak.cat.android.ui.theme.CatViewerDemoTheme
 
-fun greet(): String {
-    return Greeting().greeting()
-}
+class MainActivity : ComponentActivity() {
 
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            CatViewerDemoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    GreetingUI(Greeting().greeting())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingUI(greeting: String) {
+    Text(text = greeting)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CatViewerDemoTheme {
+        GreetingUI("Hello Android!")
     }
 }
