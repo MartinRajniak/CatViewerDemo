b6959ac5a62b54b400330415fbedde4614476446
Martin Rajniak <martin.rajniak@avast.com>
2021-12-22 19:55:59 +0100

    Downgrade AGP to 7.0.4
    This will allow users to run project
    in Android Studio Arctic Fox.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
-        classpath("com.android.tools.build:gradle:7.1.0-beta04")
+        classpath("com.android.tools.build:gradle:7.0.4")
         classpath("com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:0.11.0")
     }
 }
