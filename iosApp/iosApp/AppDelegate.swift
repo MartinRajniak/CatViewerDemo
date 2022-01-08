bc3933602ae999bc600a94ff8c90705d441633ee
Martin Rajniak <rajniak.m@gmail.com>
2022-01-08 09:20:45 +0100

    Add Crashlytics support to iOS

---
+++ iosApp/iosApp/AppDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
